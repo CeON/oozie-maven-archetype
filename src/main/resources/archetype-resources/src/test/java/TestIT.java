d24ee5376d0089e687e8cb61482c3f05822e94b7
krzywojc <krzywojc@icm.edu.pl>
2013-06-02 20:35:57 +0200

    Archetype updated to suport integration tests in the plugin

---
+++ src/main/resources/archetype-resources/src/test/java/TestIT.java
@@ -0,0 +1,33 @@
+package $package;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestIT {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
